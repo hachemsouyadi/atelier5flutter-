8df2188b036e9c900f46bbe6030e5a4f40ec7cd2
Hachem Souyadi <hachemdsq@gmail.com>
2025-11-25 13:28:44 -0500

    nouvelle modif

---
+++ lib/page_infos.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class PageInfos extends StatelessWidget {
+  const PageInfos({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+ home: Scaffold(
+appBar: AppBar(title: Text("Page d’infos")),
+ body: Center(
+ child: Column(
+ mainAxisAlignment: MainAxisAlignment.center,
+ children: [
+ Text("Bienvenue sur la page la page Infos !"),
+ ElevatedButton(
+onPressed: () {
+Navigator.pop(context);
+},
+child: Text("Page précédente"),
+),
+ ],
+ ),
+ ),
+ ),
+ );
+}
+}
