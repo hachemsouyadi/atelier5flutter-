b572ea2fa4db87cdf8250bac645438fbb1107e37
Hachem Souyadi <hachemdsq@gmail.com>
2025-11-25 13:34:33 -0500

    nouvelle modif

--- lib/page_accueil.dart
+++ lib/page_accueil.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'page_infos.dart';
 
 class PageAccueil extends StatelessWidget {
   const PageAccueil({super.key});
@@ -13,6 +14,16 @@ appBar: AppBar(title: Text("Page d'accueil")),
  mainAxisAlignment: MainAxisAlignment.center,
  children: [
  Text("Bienvenue sur la page d'accueil !"),
+ ElevatedButton(
+onPressed: () {
+Navigator.push(
+context,
+MaterialPageRoute(builder: (context) => PageInfos()),
+);
+},
+child: Text("Allez à la page Infos"),
+),
+
  ],
  ),
  ),
